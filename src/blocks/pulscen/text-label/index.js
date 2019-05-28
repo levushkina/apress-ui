32f8da89ba6f7a9c62d18523e91dc4f6bb108186
Levushkina Marina <nara-yan@bk.ru>
2019-05-28 14:26:02 +0500

    feature(bulscen): add labels
    https://jira.railsc.ru/browse/PC4-23481

---
+++ src/blocks/pulscen/text-label/index.js
@@ -0,0 +1,5 @@
+import './text_label_common.css';
+import './text_label_positive.css';
+import './text_label_neutral.css';
+import './text_label_highlighted.css';
+import './text_label_support.css';
